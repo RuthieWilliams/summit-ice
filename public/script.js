6eb8673fca982c28d954b32f28f835dc4f9c6691
RuthieWilliams <ruth.elizabeth.williams@gmail.com>
2019-09-11 10:14:52 -0400

    form validation added

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 function validateForm() {
-  var x = document.forms["signupForm"]["email"].value;
-  if (x == "") {
-    alert("Please provide a valid email address");
-    return false;
+    
+var x = document.forms["signupForm"]["email"].value;
+if (x==null || x=="")
+  {
+  alert("Please enter your email address");
+  return false;
   }
-}
+
+var y = document.forms["signupForm"]["password"].value;
+  {
+if (y==null || y=="")
+  alert("Please enter your password");
+  return false;
+  }
+    
+}    
